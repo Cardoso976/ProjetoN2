8970052f737c38aeeb6faeb3403bc46078d483d7
Rodrigo <rodrigo_cardoso97@hotmail.com>
2018-06-12 23:55:17 -0300

    view produto

--- ClientApp/app/components/produto/view-produto/view-produto.component.ts
+++ ClientApp/app/components/produto/view-produto/view-produto.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { ToastyService } from 'ng2-toasty';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ProdutoService } from './../../../services/produto.service';
+import { Component, OnInit, NgZone } from '@angular/core';
 
 @Component({
   selector: 'app-view-produto',
@@ -6,10 +9,50 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./view-produto.component.css']
 })
 export class ViewProdutoComponent implements OnInit {
+  produto : any;
+  produtoId: number = 0;
 
-  constructor() { }
+  constructor(
+    private produtoService : ProdutoService,
+    private zone: NgZone,
+    private route: ActivatedRoute, 
+    private router: Router,
+    private toasty: ToastyService) { 
+    route.params.subscribe(p => {
+      this.produtoId = +p['id'];
+      if (isNaN(this.produtoId) || this.produtoId <= 0) {
+        router.navigate(['/produtos']);
+        return; 
+      }
+    });
+   }
 
-  ngOnInit() {
+   ngOnInit() {     
+    this.produtoService.getProduto(this.produtoId)
+      .subscribe(
+        m => this.produto = m,
+        err => {
+          if (err.status == 404) {
+            this.router.navigate(['/produtos']);
+            return; 
+          }
+        });
+  }
+
+  delete() {
+    if (confirm("Tem certeza?")) {
+      this.produtoService.delete(this.produto.id)
+        .subscribe(x => {
+          this.toasty.warning({
+            title: 'Sucesso', 
+            msg: 'Produto excluido!!.',
+            theme: 'bootstrap',
+            showClose: true,
+            timeout: 2000
+          });
+          this.router.navigate(['/produtos']);
+        });
+    }
   }
 
 }
